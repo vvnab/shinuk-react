b9a8b6cc221a5a5ca13e62d4a33ebf3419ba949d
Виталий Набережный <naberezhniy@taxi21.ru>
2019-03-11 17:23:59 +0300

    change logo

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          <h2>S H I N U K</h2>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.tsx</code> and save to reload.
+            Когда-нибудь здесь будет сайт&hellip; ))
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
---
+++ src/pages/store.ts
@@ -0,0 +1,10 @@
+const config: object = {
+  apiKey: "AIzaSyBCZe4Ip_1mguMB8gad9s4m71mfMlEw55o",
+  authDomain: "shinuk-891a5.firebaseapp.com",
+  databaseURL: "https://shinuk-891a5.firebaseio.com",
+  projectId: "shinuk-891a5",
+  storageBucket: "shinuk-891a5.appspot.com",
+  messagingSenderId: "817368288412"
+};
+
+export default config;
